bd9f8da3c2bc13101a86d6dfefd7a2fd46d60ae3
gifarz <gifarz06@gmail.com>
2019-11-11 20:27:14 +0700

    cermatitest

--- src/component/Cookie.jsx
+++ src/component/Cookie.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import CookieConsent from 'react-cookie-consent';
+import './css/style.css';
 
 const Cookie = () => (
     <div className="Cookies">
         <CookieConsent
-            className="cookie-consent"
             location="top"
             buttonText="Got it"
             style={{ 
+                fontFamily: "Verdana, Tahoma, sans-serif",
                 width: "100%",
-                gridGap: "10px",
                 background: "#f5f5f5",
                 color: "#000",
-                textAlign: "justify",
-                fontSize: "15px"
+                fontSize: "14px"
             }}
             buttonStyle={{
                 position: "relative",
@@ -27,8 +26,11 @@ const Cookie = () => (
             }}
             expires={150}
         >
-            By accessing and using this website, you acknowledge that you have read and
-            understand our <span style={{color: "#007bc1"}}> Cookie Policy, Privacy Policy,</span> and our <span style={{color: "#007bc1"}}> Terms of Service. </span>
+            By accessing and using this website, you acknowledge that you have read and understand our 
+            <a href="#" style={{textDecoration: "none"}}> Cookie Policy</a>, 
+            <a href="#" style={{textDecoration: "none"}}> Privacy,</a> 
+            <a href="#" style={{textDecoration: "none"}}> Policy</a>, and our 
+            <a href="#" style={{textDecoration: "none"}}> Terms of Service</a>.
         </CookieConsent>
     </div>
 )
